5865e2b774f7180b5c332773f361ce49bbffd51b
Trần Trung Hiếu <97189417+Nonsense12345@users.noreply.github.com>
2023-08-13 17:59:09 +0700

    Update +page.server.ts

--- src/routes/book/[id=id]/[chapterid]&[v_id]/+page.server.ts
+++ src/routes/book/[id=id]/[chapterid]&[v_id]/+page.server.ts
@@ -24,7 +24,7 @@ export const actions = {
 		const dict = formData.get('dict');
 		if (dict == 'Names') {
 			fs.appendFile(
-				'.'+ Names,
+				process.cwd() + Names,
 				`\n${zh}=${vi}`,
 				() => {
 					console.log('Done!!');
@@ -33,7 +33,7 @@ export const actions = {
 			addName(zh, vi);
 		} else {
 			fs.appendFile(
-				'.'+ VP,
+				process.cwd()+ VP,
 				`\n${zh}=${vi}`,
 				() => {
 					console.log('Done!!');
