b2ac2024abb768021983997b867d8e397b198f0b
Trần Trung Hiếu <97189417+Nonsense12345@users.noreply.github.com>
2023-08-13 17:30:12 +0700

    Update LookUp.ts

--- src/lib/server/LookUp.ts
+++ src/lib/server/LookUp.ts
@@ -1,22 +1,31 @@
-import Names from '$lib/server/assets/Names.txt?raw';
-import VP from '$lib/server/assets/VietPhrase.txt?raw';
-import LV from '$lib/server/assets/LacViet.txt?raw';
+import Names from '$lib/server/assets/Names.txt?url';
+import VP from '$lib/server/assets/VietPhrase.txt?url';
+import LV from '$lib/server/assets/LacViet.txt?url';
+import readline from 'readline';
+import fs from 'fs';
+import path from 'path';
 let Dicts: any;
 // const Names = './src/lib/server/assets/Names.txt';
 // const VP = './src/lib/server/assets/VietPhrase.txt';
 // const LV = './src/lib/server/assets/LacViet.txt';
-function initDict() {
+async function initDict() {
 	Dicts = {
-		names: GenDictMap(Names),
-		VP: GenDictMap(VP),
-		LV: GenDictMap(LV)
+		names: await GenDictMap(Names),
+		VP: await GenDictMap(VP),
+		LV: await GenDictMap(LV)
 	};
 }
 
-function GenDictMap(data: string) {
+async function GenDictMap(file_path: string) {
+	let new_path = path.join(process.cwd(), '.svelte-kit', 'output', 'server', file_path);
+	const fileStream = fs.createReadStream(new_path);
+	const rl = readline.createInterface({
+		input: fileStream,
+		crlfDelay: Infinity
+	});
 	let map = new Map() as Map<string, string>;
-	const readLine = data.split('\n');
-	for (const line of readLine) {
+
+	for await (const line of rl) {
 		const [key, value] = line.split('=') as [string, string];
 		map.set(key, value);
 	}
